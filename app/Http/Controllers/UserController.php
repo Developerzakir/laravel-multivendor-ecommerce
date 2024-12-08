dd3a7d9c0834f184ce3ad963a92e900edeee3898
zakir Hossen <zakirhossen082@gmail.com>
2024-12-08 10:15:52 +0600

    user login, logout  notification display

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -66,6 +66,11 @@ class UserController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        $notification = array(
+            'message' => 'User Logout Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect('/login')->with($notification);
     } // End userLogout Mehtod
 }
