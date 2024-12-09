e8203fca33f6cf6433e7ef5501633024ee91a22b
zakir Hossen <zakirhossen082@gmail.com>
2024-12-09 11:00:43 +0600

    brand field  validation with javascript

--- resources/views/admin/inc/scripts.blade.php
+++ resources/views/admin/inc/scripts.blade.php
@@ -20,6 +20,7 @@
 	  <script src="{{asset('backend/assets/js/index.js')}}"></script>
 	<!--app JS-->
 	<script src="{{asset('backend/assets/js/app.js')}}"></script>
+	<script src="{{asset('backend/assets/js/validate.min.js')}}"></script>
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script>
