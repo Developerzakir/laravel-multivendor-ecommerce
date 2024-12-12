7812491b6e787df2afa08f1570bd6bdedc15abb3
zakir Hossen <zakirhossen082@gmail.com>
2024-12-12 07:52:03 +0600

    read vendor,category,brand on add product page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Brand;
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -13,8 +16,12 @@ class ProductController extends Controller
         return view('admin.product.index',compact('products'));
     } //End method
 
-    public function addProduct(){
+    public function addProduct()
+    {
+        $activeVendor = User::where('status','active')->where('role','vendor')->latest()->get();
+        $brands = Brand::latest()->get();
+        $categories = Category::latest()->get();
 
-        return view('admin.product.create');
+        return view('admin.product.create',compact('brands','categories','activeVendor'));
     } // End Method 
 }
