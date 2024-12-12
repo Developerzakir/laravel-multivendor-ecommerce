da70f4621650df957d37b900c9a0a2387077eb2e
zakir Hossen <zakirhossen082@gmail.com>
2024-12-12 09:25:38 +0600

    append subcategory based on select parent category, with jquery ajax

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -100,15 +100,12 @@
 						 @endforeach
 					  </select>
 				  </div>
-				  {{-- <div class="col-12">
+				  <div class="col-12">
 					<label for="inputCollection" class="form-label">Product SubCategory</label>
 					<select name="subcategory_id" class="form-select" id="inputCollection">
 						<option></option>
-						<option value="1">One</option>
-						<option value="2">Two</option>
-						<option value="3">Three</option>
 					  </select>
-				  </div> --}}
+				  </div>
 				  <div class="col-12">
 					<label for="inputCollection" class="form-label">Select Vendor</label>
 					<select name="vendor_id" class="form-select" id="inputCollection">
@@ -210,6 +207,34 @@
   });
    
   </script>
+
+
+</script>
+
+ <!--Subcategory display js code based on category select -->
+<script type="text/javascript">
+		$(document).ready(function(){
+			$('select[name="category_id"]').on('change', function(){
+				var category_id = $(this).val();
+				if (category_id) {
+					$.ajax({
+						url: "{{ url('/subcategory/ajax') }}/"+category_id,
+						type: "GET",
+						dataType:"json",
+						success:function(data){
+							$('select[name="subcategory_id"]').html('');
+							var d =$('select[name="subcategory_id"]').empty();
+							$.each(data, function(key, value){
+								$('select[name="subcategory_id"]').append('<option value="'+ value.id + '">' + value.subcategory_name + '</option>');
+							});
+						},
+					});
+				} else {
+					alert('danger');
+				}
+			});
+		});
+</script>
 
 
 @endsection
